26ccf51812ccd2db7625d5ab301eeeb3220d6627
Adri <apereg24@estudiantes.unileon.es>
2020-05-04 02:55:38 +0200

    Implementada unordered
    Listo para el debug :)

--- ed-a004-2020/src/ule/edi/recursiveList/UnorderedLinkedListImpl.java
+++ ed-a004-2020/src/ule/edi/recursiveList/UnorderedLinkedListImpl.java
@@ -18,23 +18,55 @@ public class UnorderedLinkedListImpl<T> extends AbstractLinkedListImpl<T> implem
 	
 	@Override
 	public void addFirst(T element) {
-     //TODO
+		if(element == null)
+			throw new NullPointerException();
+     	if(this.isEmpty())
+     		this.front = new Node<T>(element);
+     	else {
+     		Node<T> aux = this.front;
+     		this.front = new Node<T>(element);
+     		this.front.next = aux;
+     	} 		
 	}
 	
 	
 	@Override
 	public void addLast(T element) {
-		// TODO RECURSIVO
-		
-	
+		if(element == null)
+			throw new NullPointerException();
+		if(this.isEmpty())
+			this.addFirst(element);
+		this.addLastRec(this.front, element);	
 	}	
 
 	
+	private void addLastRec(Node<T> node, T element) {
+		if(node.next == null)
+			node.next = new Node<T>(element);
+		else
+			this.addLastRec(node.next, element);
+	}
+
 	@Override
 	public void addBefore(T element, T target) {
-		// TODO RECURSIVO
-			
-			
+		if(element == null || target == null)
+			throw new NullPointerException();
+		if(!this.contains(target))
+			throw new NoSuchElementException();
+		this.addBeforeRec(this.front, element, target);	
+	}
+
+	private void addBeforeRec(Node<T> node, T element, T target) {
+		if(node.next == null) {
+			if(node.elem.equals(target))
+				this.addFirst(element);
+		} else if(node.next.elem.equals(target)) {
+				Node<T> aux = new Node<T>(element);
+				aux.next = node.next.next;
+				node.next = aux;
+		} else {
+			this.addBeforeRec(node.next, element, target);
+		}
 	}
 
 		
