d6755d4e85b7a0dfec8c9e07e39f1248097b02ea
Adri <apereg24@estudiantes.unileon.es>
2020-05-05 14:17:26 +0200

    Ordered comentada

--- ed-a004-2020/src/ule/edi/recursiveList/OrderedLinkedListImpl.java
+++ ed-a004-2020/src/ule/edi/recursiveList/OrderedLinkedListImpl.java
@@ -6,11 +6,11 @@ public class OrderedLinkedListImpl<T extends Comparable<? super T>> extends Abst
 		implements OrderedListADT<T> {
 
 	public OrderedLinkedListImpl() {
-		// VacÃ­a
+		/* Lista vacía */
 	}
 
 	public OrderedLinkedListImpl(T... v) {
-		// AÃ±ade todos los elementos del array 'v'
+		/* Se añaden todos los elementos del array 'v' */
 		for (T Vi : v) {
 			add(Vi);
 		}
@@ -18,34 +18,48 @@ public class OrderedLinkedListImpl<T extends Comparable<? super T>> extends Abst
 
 	@Override
 	public void add(T element) {
+		/* Se lanzan las excepciones en los casos contemplados en la documentacion */
 		if (element == null)
 			throw new NullPointerException();
 
-		if (this.isEmpty()) 
+		/*
+		 * Si la lista esta vacia se modifica el front si no se llama al metodo
+		 * recursivo
+		 */
+		if (this.isEmpty())
 			this.front = new Node<T>(element);
-		else 
+		else
 			this.addRec(this.front, element);
 	}
 
 	private void addRec(Node<T> node, T element) {
 		if (node.next == null) {
-			if(node.elem.compareTo(element) > 0) {
+			/* Si el siguiente es nulo ya no se sigue */
+			if (node.elem.compareTo(element) > 0) {
+				/*
+				 * Que el anterior sea mayor esta contemplado siempre menos en un caso, el
+				 * primero
+				 */
 				Node<T> aux = new Node<T>(element);
 				aux.next = node;
 				this.front = aux;
-			} else
+			} else {
+				/* Se añade al final de la lista */
 				node.next = new Node<T>(element);
+			}
 		} else {
-			if(node.elem.compareTo(element) > 0) {
+			if (node.elem.compareTo(element) > 0) {
+				/* Si el elemento es mayor se mete detras */
 				Node<T> aux = new Node<T>(element);
 				aux.next = node;
 				this.front = aux;
-			}
-			else if (node.next.elem.compareTo(element) > 0) {
+			} else if (node.next.elem.compareTo(element) > 0) {
+				/* Si no es mayor el elemento pero si el siguiente se mete entre medias */
 				Node<T> newNode = new Node<>(element);
 				newNode.next = node.next;
 				node.next = newNode;
 			} else {
+				/* Si no se cumple nada se avanza recursivamente al siguiente nodo */
 				this.addRec(node.next, element);
 			}
 		}
@@ -53,19 +67,27 @@ public class OrderedLinkedListImpl<T extends Comparable<? super T>> extends Abst
 
 	@Override
 	public int removeDuplicates() throws EmptyCollectionException {
+		/* Se lanzan las excepciones en los casos contemplados en la documentacion */
 		if (this.isEmpty())
 			throw new EmptyCollectionException("AbstractLinkedList");
+
+		/* Llamada al metodo recursivo */
 		return this.removeDuplicatesRecOrdered(this.front);
 	}
 
 	private int removeDuplicatesRecOrdered(Node<T> node) {
+		/* Si el siguiente es nulo ya se llego al final */
 		if (node.next == null)
 			return 0;
+		/* Si el nodo es el mismo que el siguiente */
 		if (node.elem.compareTo(node.next.elem) == 0) {
+			/* Se elimina el siguiente */
 			node.next = node.next.next;
+			/* Se vuelve a llamar con el mismo nodo para comprobar el nuevo siguiente */
 			return 1 + removeDuplicatesRecOrdered(node);
 		}
 
+		/* Si no se cumple se avanza recursivamente al siguiente nodo */
 		return removeDuplicatesRecOrdered(node.next);
 	}
 
